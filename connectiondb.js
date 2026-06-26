8da860ef9b80e36288f2f0b0a5f00c856d7d4891
elmonoshine-max <elmonoshine@gmail.com>
2026-06-25 20:26:27 -0500

    Delete connectiondb.js

--- connectiondb.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-const URI = 'mongodb+srv://SantiagoLS:bZ7Ch8t85jWqCxxD@santiagols.xejbz4f.mongodb.net/prueba107'
-mongoose.connect(URI)
-
-
-module.exports= mongoose;

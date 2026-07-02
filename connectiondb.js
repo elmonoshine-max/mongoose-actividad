3ce42f3dc157a156b5654d99ceac35fc2ed40818
sebas <sebistrapo@gmail.com>
2026-07-01 20:01:21 -0500

    Segunda oportunidad

--- connectiondb.js
+++ connectiondb.js
@@ -1,6 +1,7 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
 
-const URI = 'mongodb+srv://SantiagoLS:bZ7Ch8t85jWqCxxD@santiagols.xejbz4f.mongodb.net/prueba107'
+const URI = 'mongodb+srv://SantiagoLS:bZ7Ch8t85jWqCxxD@santiagols.xejbz4f.mongodb.net/prueba107';
 mongoose.connect(URI)
 
 
